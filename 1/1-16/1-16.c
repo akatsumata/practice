130a6f8a31d4d0df07ef281dbe8ecd731b4e6266
勝又明子 <akatsumata@nig.ac.jp>
2019-10-10 10:47:33 +0900

    add 1-16.c

--- 1/1-16/1-16.c
+++ 1/1-16/1-16.c
@@ -9,45 +9,47 @@ void copy(char to[], char from[]);
  
 int main()
 {
-    int len;
-    int max;
-    char line[MAXLINE];
-    char longest[MAXLINE];
+    	int len;
+    	int max;
+    	char line[MAXLINE];
+    	char longest[MAXLINE];
  
-    max = 0;
-    while((len = l_getline(line, MAXLINE)) > 0){
-        if(len > max){
-            max = len;
-            copy(longest, line);
-        }
-        printf("入力行群の長さ = %d\n", len);
-        printf("入力したテキスト = %s\n", line);
-    }
-    return 0;
+	max = 0;
+    	while((len = l_getline(line, MAXLINE)) > 0){
+        	if(len > max){
+            		max = len;
+            		copy(longest, line);
+        	}
+        	printf("入力行群の長さ = %d\n", len);
+        	printf("入力したテキスト = %s\n", line);
+    	}
+    	return 0;
 }
  
  
 int l_getline(char s[], int lim)
 {
-    int c, i;
+    	int c, i;
  
-    for(i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
-        s[i] = c;
-        if(c == 'n') {
-            s[i] = c;
-            ++i;
-        }
-    s[i] = '\0';
-    return i;
+    	for(i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i){
+        	s[i] = c;
+        	if(c == 'n') {
+            		s[i] = c;
+            		++i;
+        	}
+	}
+    	s[i] = '\0';
+    	return i;
 }
  
  
 void copy(char to[], char from[])
 {
-    int i;
+    	int i;
  
-    i = 0;
-    while((to[i] = from[i]) != '\0')
-        ++i;
+    	i = 0;
+   	while((to[i] = from[i]) != '\0'){
+        	++i;
+	}
 }
 
