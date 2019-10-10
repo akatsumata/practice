5b364cfa0638c1441f33d946d8f39f7a44226d22
勝又明子 <akatsumata@nig.ac.jp>
2019-10-10 13:36:01 +0900

    add 2

---
+++ 2/2-7/2-7.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+unsigned invert(unsigned x, int p , int n);
+ 
+int main()
+{
+    	unsigned x;
+    	int p, n;
+ 
+    	x = 32;
+    	p = 4;
+    	n = 3;
+    	x = (x, p ,n);
+    	printf("%d\n", x);
+    	return 0;
+}
+ 
+unsigned invert(unsigned x, int p , int n)
+{
+    	return x ^ (~(~0 << n) << (p + 1 - n));
+}
+
