cb4d50a7ce1cc4c9bd408c33460a963531e6a58c
勝又明子 <akatsumata@nig.ac.jp>
2019-10-10 10:29:23 +0900

    add 1

---
+++ 1/kr122_2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+ 
+#define MAXLINE 1000
+#define MAXLEN 5           /* 文字列5カウントしたら折り返し */
+ 
+int l_getline(char line[], int lim);
+ 
+int main()
+{
+    int len;
+    char line[MAXLINE];
+ 
+    while((len = l_getline(line, MAXLINE)) > 0) {
+          printf("%s\n", line);
+     }
+     return 0;
+}
+ 
+ 
+int l_getline(char line[], int lim)
+{
+    int c, i;
+    int nc;
+    nc = 0;
+    
+    for(i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+        if(nc >= MAXLEN || c == '\t' || c == ' ') {
+          line[i] = c;
+          ++i;
+          line[i] = '\n';
+          nc =0;
+         }
+         else {
+            line[i] = c;
+            ++nc;
+         }
+      }
+ 
+    if(c == '\n') {
+       line[i] = c;
+       ++i;
+    }
+    line[i] = '\0';
+ 
+    return i;
+}
+
